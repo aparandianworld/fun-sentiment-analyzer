a0df835defa87b364f92ca835d09b5c435107087
Aaron Parandian <aparandian@apple.com>
2025-10-05 16:54:58 -0700

    aparandian-patch-04. add single text sentiment analysis

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ app = FastAPI(title = "Fun Sentiment Analyzer API")
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using device: {device}")
 
-sentiment_pipeline = pipeline("sentiment-analysis", model = "cardiffnlp/twitter-roberta-base-sentiment-latest", device = device)
+sentiment_pipeline = pipeline(
+    "sentiment-analysis", 
+    model = "cardiffnlp/twitter-roberta-base-sentiment-latest", 
+    device = device,
+)
 
 # Temporary test
 # test_text = "This sentiment analyzer API is awesome!"
@@ -24,3 +28,17 @@ class TextInput(BaseModel):
 
 class BatchTextInput(BaseModel):
     texts: List[str]
+
+# API endpoint for single text sentiment analysis
+@app.post("/analyze")
+async def analyze_sentiment(input: TextInput):
+    try:
+        result = sentiment_pipeline(input.text)
+        print(f"Result: {result}")
+        print("---\n")
+        return {"label": result[0]["label"], "confidence": result[0]["score"]}
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail = str(e))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = "0.0.0.0", port = 8000)
